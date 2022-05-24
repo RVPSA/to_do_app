c6aebae2d9aec3b513207194cf4dd3a7e03333a9
sachinthaakesh <akeshramasinghe@gmail.com>
2022-05-24 22:03:31 +0530

    update script. js
    take values from html side

---
+++ script.js
@@ -0,0 +1,4 @@
+var inputElement = document.querySelector("input");
+var formElement = document.querySelector("form");
+var listElement = document.querySelector("ul");
+var spanElement = document.querySelector("#span1");
