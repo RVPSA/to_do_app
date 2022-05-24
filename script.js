f7298d57552a59038255f3d6ebfaef43e7e7aa0c
sachinthaakesh <akeshramasinghe@gmail.com>
2022-05-24 22:20:27 +0530

    Update script.js
    make a array which consists of some tasks and develop 'listPopulate' mehod

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ var inputElement = document.querySelector("input");
 var formElement = document.querySelector("form");
 var listElement = document.querySelector("ul");
 var spanElement = document.querySelector("#span1");
+
+var tasks = ['Go to shop','Settle Electricity bill'];
+
+function listPopulate(){
+    tasks.forEach(function(item){
+        var newLi = document.createElement("li");
+        //add span tag for text
+        var spanEle = document.createElement("span");
+        spanEle.innerHTML = item;
+        newLi.appendChild(spanEle);
+        
+        //add anchor tag for delete
+        var anchorEle = document.createElement('a');
+        anchorEle.classList.add('Delete');
+        newLi.appendChild(anchorEle);
+
+        listElement.appendChild(newLi);
+    })
+}
+
+listPopulate();
